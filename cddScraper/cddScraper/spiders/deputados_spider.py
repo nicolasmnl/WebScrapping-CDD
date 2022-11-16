8d3188fcbffc37ff908e405a3ef35abd4c360c75
Nicolas Moreira Nobre Leite <nicolas.leite@ccc.ufcg.edu.br>
2022-11-16 16:41:56 -0300

    Web Scrapping finalizado

---
+++ cddScraper/cddScraper/spiders/deputados_spider.py
@@ -0,0 +1,47 @@
+import scrapy
+from .utils import get_links_deputados,build_csv, get_csv_header, get_nome, get_info_plenario_comissao, get_data_nascimento, get_gastos_par_gab, get_salario_bruto, get_qtd_viagens
+import csv
+
+info_deputado = []
+class DeputadosSpider(scrapy.Spider):
+    name = "miles_morales"
+
+    def start_requests(self):
+        
+        urls = get_links_deputados()
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        filename = 'deputado.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
+        
+        # Retorna o nome da deputada
+        nome = get_nome(response)
+        print(nome)
+        genero = 'M'
+        # Retorna todas as informações de plenario e comissao (presenca, ausencia, etc)
+        info_plen_com = get_info_plenario_comissao(response)
+
+        # Retorna a data de nascimento da deputada
+
+        data_nascimento = get_data_nascimento(response)
+
+        gastos_par_gab = get_gastos_par_gab(response)
+
+        salario = get_salario_bruto(response)
+        
+        qtd_viagens = get_qtd_viagens(response)
+
+        info_deputado.append(build_csv(nome, genero, data_nascimento, info_plen_com, gastos_par_gab, salario, qtd_viagens))
+        
+        
+        
+        header = get_csv_header()
+        with open('info_deputados.csv', 'w') as file:
+            writer = csv.writer(file)
+            writer.writerow(header)
+        
+            writer.writerows(info_deputado)
